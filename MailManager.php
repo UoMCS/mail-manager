3f21e854c1167a61a091879148976d8283fb3cc6
Paul Waring <paul.waring@manchester.ac.uk>
2013-12-13 11:21:02 +0000

    Fix whitespace

--- MailManager.php
+++ MailManager.php
@@ -12,10 +12,10 @@ class MailManager
   {
     $this->connection = new mysqli($this->dbhost, $username, $password, $dbname);
 	
-	if ($connection->connect_error)
-	{
-	  throw new Exception('Could not connect to database');
-	}
+    if ($connection->connect_error)
+    {
+      throw new Exception('Could not connect to database');
+    }
   }
   
   public function setSubject($subject)
@@ -41,22 +41,22 @@ class MailManager
   public function addRecipient($emailAddress)
   {
     if (filter_var($emailAddress, FILTER_VALIDATE_EMAIL))
-	{
-	  list($user, $domain) = explode('@', $emailAddress);
+    {
+      list($user, $domain) = explode('@', $emailAddress);
 	  
-	  if (in_array($domain, $this->permittedDomains))
-	  {
-	    $this->recipients[] = $emailAddress;
-	  }
-	  else
-	  {
-	    throw new Exception('Recipient does not fall within list of permitted domains');
-	  }
-	}
-	else
-	{
-	  throw new Exception('Invalid recipient');
-	}
+      if (in_array($domain, $this->permittedDomains))
+      {
+        $this->recipients[] = $emailAddress;
+      }
+      else
+      {
+        throw new Exception('Recipient does not fall within list of permitted domains');
+      }
+    }
+    else
+    {
+      throw new Exception('Invalid recipient');
+    }
   }
   
   /**
@@ -65,32 +65,30 @@ class MailManager
   public function validate()
   {
     // Basic checks:
-	// 1. Do we have at least one recipient?
-	if (count($this->recipients()) < 1)
-	{
-	  throw new Exception('No recipients specified');
-	}
+    // 1. Do we have at least one recipient?
+    if (count($this->recipients()) < 1)
+    {
+      throw new Exception('No recipients specified');
+    }
 	
-	// 2. Do we have a subject?
-	if (empty($this->subject))
-	{
-	  throw new Exception('No subject specified');
-	}
+    // 2. Do we have a subject?
+    if (empty($this->subject))
+    {
+      throw new Exception('No subject specified');
+    }
 	
-	// 3. Do we have a message body?
-	if (empty($this->body))
-	{
-	  throw new Exception('No message body specified');
-	}
-	
-	
+    // 3. Do we have a message body?
+    if (empty($this->body))
+    {
+      throw new Exception('No message body specified');
+    }
   }
   
   public function send()
   {
     $this->validate();
 	
-	// Debugging for the moment
-	print 'Email sent';
+    // Debugging for the moment
+    print 'Email sent';
   }
 }
