f9819b2fda4a743c27a05b28475a3f5bdb4116e8
Paul Waring <paul.waring@manchester.ac.uk>
2013-12-16 10:39:38 +0000

    Catch exceptions in example file

--- example.php
+++ example.php
@@ -5,10 +5,17 @@ date_default_timezone_set('Europe/London');
 require_once 'config.inc.php';
 require_once 'MailManager.php';
 
-$mm = new MailManager($database_host, $database_user, $database_pass, $database_name, true);
+try
+{
+  $mm = new MailManager($database_host, $database_user, $database_pass, $database_name, true);
 
-$mm->setSubject('Test Email');
-$mm->addRecipient('paul.waring@manchester.ac.uk');
-$mm->setBody('Test Body');
+  $mm->setSubject('Test Email');
+  $mm->addRecipient('paul.waring@manchester.ac.uk');
+  $mm->setBody('Test Body');
 
-$mm->send();
+  $mm->send();
+}
+catch (Exception $e)
+{
+  
+}
