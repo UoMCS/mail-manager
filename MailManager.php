b031f6aab4f33d07c06e6610a5f65f34a8b35bc5
Paul Waring <paul.waring@manchester.ac.uk>
2013-12-16 16:23:55 +0000

    Remove unused code
    This has all moved to web service class.

--- MailManager.php
+++ MailManager.php
@@ -7,15 +7,8 @@ class MailManager
   private $subject = '';
   private $body = '';
 
-  private $log_schema_file = 'logging.sql';
-  private $log_table = 'mail_manager_log';
   private $enableMail = false;
-  
-  private $permittedDomains = array('manchester.ac.uk');
-  private $maxRecipientsOneMessage = 6;
-
-  private $mysqlDateTimeFormat = 'Y-m-d H:i:s';
-  
+    
   private $fromAddress;
   
   private $additionalHeaders;
@@ -23,39 +16,12 @@ class MailManager
 
   public function __construct($dbhost, $username, $password, $dbname, $enableMail = false)
   {
-    $this->connection = new mysqli($dbhost, $username, $password, $dbname);
-	
-    if ($this->connection->connect_error)
-    {
-      throw new Exception('Could not connect to database');
-    }
-	
 	$this->enableMail = $enableMail;
 		
 	$this->fromAddress = 'paul.waring@manchester.ac.uk';
 	
 	$additionalHeaders = $this->fromAddress;
 	$additionalParameters = '-f' . $this->fromAddress;
-  }
-  
-  private function getCurrentTime()
-  {
-    return date($this->mysqlDateTimeFormat);
-  }
-
-  private function countEmailsSent()
-  {
-    $sql = 'SELECT id FROM ' . $this->log_table . ' WHERE log_time < ?';
-    $statement = $this->connection->prepare($sql);
-    $statement->bind_param('s', $this->rateLimitCutoff);
-    $statement->execute();
-    $result = $statement->get_result();
-
-    $emailsSent = $result->num_rows;
-
-    $statement->close();
-
-    return $emailsSent;
   }
   
   public function setSubject($subject)
@@ -117,12 +83,6 @@ class MailManager
     if (empty($this->body))
     {
       throw new Exception('No message body specified');
-    }
-
-    // 4. Rate limit number of recipients
-    if (count($this->recipients) > $this->maxRecipientsOneMessage)
-    {
-      throw new Exception('Too many recipients, maximum allowed: ' . $this->maxRecipientsOneMessage);
     }
   }
   
