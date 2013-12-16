58fb96feb18cf35d6b7ff0dbd07d2d5c908fa258
Paul Waring <paul.waring@manchester.ac.uk>
2013-12-16 10:31:59 +0000

    Set subject and body in 'too many recipients' test
    Subject and body are tested first, so if they are not set the 'too many
    recipients' exception will never be triggered.

--- exceptions.php
+++ exceptions.php
@@ -5,6 +5,7 @@ date_default_timezone_set('Europe/London');
 require_once 'config.inc.php';
 require_once 'MailManager.php';
 
+// Recipient not in list of permitted domains
 try
 {
   $mm = new MailManager($database_host, $database_user, $database_pass, $database_name);
@@ -15,9 +16,14 @@ catch (Exception $e)
   print $e->getMessage() . "\n";
 }
 
+// Too many recipients
 try
 {
   $mm = new MailManager($database_host, $database_user, $database_pass, $database_name);
+  
+  $mm->setSubject('Test');
+  $mm->setBody('Body');
+  
   $mm->addRecipient('doesnotexist@manchester.ac.uk');
   $mm->addRecipient('doesnotexist@manchester.ac.uk');
   $mm->addRecipient('doesnotexist@manchester.ac.uk');
