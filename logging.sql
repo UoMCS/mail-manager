f10799b962f3fa77ccc13eded9dc733cc8f5538b
Paul Waring <paul.waring@manchester.ac.uk>
2013-12-19 14:55:12 +0000

    Remove logging schema
    This is now handled by the web service.

--- logging.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE mail_manager_log
-(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  recipient VARCHAR(255) NOT NULL,
-  subject VARCHAR(255) NOT NULL,
-  body TEXT NOT NULL,
-  log_time DATETIME NOT NULL
-);
