7f869ff7c063cde2ad6b2cee3a96f936c9e79bc2
Paul Waring <paul.waring@manchester.ac.uk>
2014-01-03 09:36:05 +0000

    Convert tabs to spaces

--- MailManager.php
+++ MailManager.php
@@ -25,11 +25,11 @@ class MailManager
   public function __construct($dbhost, $username, $password, $dbname, $enable_email = false)
   {
     $this->dbhost = $dbhost;
-	$this->username = $username;
-	$this->password = $password;
-	$this->dbname = $dbname;
-	
-	$this->enable_email = $enable_email;
+    $this->username = $username;
+    $this->password = $password;
+    $this->dbname = $dbname;
+    
+    $this->enable_email = $enable_email;
   }
   
   public function set_subject($subject)
@@ -80,74 +80,74 @@ class MailManager
     {
       throw new Exception('No recipients specified');
     }
-	
+    
     // 2. Do we have a subject?
     if (empty($this->subject))
     {
       throw new Exception('No subject specified');
     }
-	
+    
     // 3. Do we have a message body?
     if (empty($this->body))
     {
       throw new Exception('No message body specified');
     }
-	
-	// 4. Simple check for maximum number of recipients
-	if (count($this->recipients) > MM_MAX_RECIPIENTS)
-	{
-	  throw new Exception('Too many recipients, maximum allowed is: ' . MM_MAX_RECIPIENTS);
-	}
+    
+    // 4. Simple check for maximum number of recipients
+    if (count($this->recipients) > MM_MAX_RECIPIENTS)
+    {
+      throw new Exception('Too many recipients, maximum allowed is: ' . MM_MAX_RECIPIENTS);
+    }
   }
   
   private function send_individual_email($email_address)
   {
-	$parameters = array();
-	$parameters['username'] = $this->username;
-	$parameters['password'] = $this->password;
-	$parameters['host'] = $this->dbhost;
-	$parameters['dbname'] = $this->dbname;
-	
-	$parameters['recipient'] = $email_address;
-	$parameters['subject'] = $this->subject;
-	$parameters['body'] = $this->body;
-	
-	$client = curl_init(MM_WEB_SERVICE_URI);
-	curl_setopt($client, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($client, CURLOPT_POST, TRUE);
-	curl_setopt($client, CURLOPT_POSTFIELDS, $parameters);
-	
-	// Remember, with cURL there are two types of failure:
-	// 1. Failure to make the HTTP request (e.g. host name does not exist).
-	// 2. Failure status code (e.g. 4xx or 5xx).
-	$response = curl_exec($client);
-	
-	if ($response === FALSE)
-	{
-	  $error = curl_error($client);
-	  throw new Exception($error);
-	}
-	else
-	{
-	  // We managed to make the request, now check what the status was
-	  $response_headers = curl_getinfo($client);
-		
+    $parameters = array();
+    $parameters['username'] = $this->username;
+    $parameters['password'] = $this->password;
+    $parameters['host'] = $this->dbhost;
+    $parameters['dbname'] = $this->dbname;
+    
+    $parameters['recipient'] = $email_address;
+    $parameters['subject'] = $this->subject;
+    $parameters['body'] = $this->body;
+    
+    $client = curl_init(MM_WEB_SERVICE_URI);
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($client, CURLOPT_POST, TRUE);
+    curl_setopt($client, CURLOPT_POSTFIELDS, $parameters);
+    
+    // Remember, with cURL there are two types of failure:
+    // 1. Failure to make the HTTP request (e.g. host name does not exist).
+    // 2. Failure status code (e.g. 4xx or 5xx).
+    $response = curl_exec($client);
+    
+    if ($response === FALSE)
+    {
+      $error = curl_error($client);
+      throw new Exception($error);
+    }
+    else
+    {
+      // We managed to make the request, now check what the status was
+      $response_headers = curl_getinfo($client);
+        
       if ($response_headers['http_code'] == 429)
-	  {
-		throw new Exception($response);
-	  }
-	}
-	
-	curl_close($client);
+      {
+        throw new Exception($response);
+      }
+    }
+    
+    curl_close($client);
   }
   
   public function send()
   {
     $this->validate();
-	
-	foreach ($this->recipients as $recipient)
-	{
-	  $this->send_individual_email($recipient);
-	}
+    
+    foreach ($this->recipients as $recipient)
+    {
+      $this->send_individual_email($recipient);
+    }
   }
 }
